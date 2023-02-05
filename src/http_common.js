8225428d2d5f6ca62af881ae03213f13ee100d97
Kvazar3S273 <ba2h007@gmail.com>
2023-02-05 18:37:53 +0200

    remove files

---
+++ src/http_common.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// export const urlBackend = 'https://3x4.com.ua/';
+export default axios.create({
+    // baseURL: `${urlBackend}`,
+
+    baseURL: "http://localhost:3000/",
+    headers:{
+        "Content-type":"application/json"
+    }
+});
