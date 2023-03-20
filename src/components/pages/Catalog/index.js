f4bc28637f079a5b2720f17033d6e5f02a274fb9
Kvazar3S273 <ba2h007@gmail.com>
2023-03-20 23:11:35 +0200

    edit catalog page

--- src/components/pages/Catalog/index.js
+++ src/components/pages/Catalog/index.js
@@ -1,13 +1,97 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../../../../src/logoN.png";
+import {
+    faTrash,
+    faPenToSquare
+  } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Catalog = () => {
   return (
     <div>
-      <h1>
-        Сторінка Каталог
-      </h1>
+      <h1 className="text-center mt-3 mb-3">Каталог товарів</h1>
+        <div className="d-grid col-5 mx-auto mb-3">
+          <button type="button" class="btn btn-warning ">Додати новий товар</button>
+        </div>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-10">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Найменування</th>
+                <th scope="col">Категорія</th>
+                <th scope="col">Підкатегорія</th>
+                <th scope="col">Фото</th>
+                <th scope="col">Ціна</th>
+                <th scope="col">Дії</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>КТ315</td>
+                <td>Транзистор</td>
+                <td>NPN</td>
+                <td><img src={logo} class="img-fluid" alt="imag" style={{height:"40px"}}/></td>
+                <td>3.50</td>
+                <td>
+                  <i style={{ color: "#03a9f4", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faPenToSquare} /></i>
+                  <i style={{ color: "#E60026", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faTrash} /></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>PIC16F628A</td>
+                <td>Мікроконтролер</td>
+                <td>МС</td>
+                <td><img src={logo} class="img-fluid" alt="imag" style={{height:"40px"}}/></td>
+                <td>83.00</td>
+                <td>
+                  <i style={{ color: "#03a9f4", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faPenToSquare} /></i>
+                  <i style={{ color: "#E60026", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faTrash} /></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Arduino Nano</td>
+                <td>Мікроконтролер</td>
+                <td>Модуль</td>
+                <td><img src={logo} class="img-fluid" alt="imag" style={{height:"40px"}}/></td>
+                <td>105.00</td>
+                <td>
+                  <i style={{ color: "#03a9f4", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faPenToSquare} /></i>
+                  <i style={{ color: "#E60026", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faTrash} /></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>1N4148</td>
+                <td>Діод</td>
+                <td>Малопотужний</td>
+                <td><img src={logo} class="img-fluid" alt="imag" style={{height:"40px"}}/></td>
+                <td>2.00</td>
+                <td>
+                  <i style={{ color: "#03a9f4", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faPenToSquare} /></i>
+                  <i style={{ color: "#E60026", fontSize: "25px" }}>
+                  <FontAwesomeIcon icon={faTrash} /></i>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+        <div className="col-md-1"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
